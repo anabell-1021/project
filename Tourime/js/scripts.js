4568707ebcf8a79aa7c5b8909d11ab4740463e33
anabell-1021 <cow369tw@gmail.com>
2022-01-19 00:31:34 +0800

    202201190031

--- Tourime/js/scripts.js
+++ Tourime/js/scripts.js
@@ -46,7 +46,7 @@
 //     let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
 //     return { 'Authorization': Authorization, 'X-Date': GMTString };
 // }
-
+//-------------------------------------------------------------------------------
 // $(function () {
 //     $.ajax({
 //         type: 'GET',
@@ -72,7 +72,7 @@
 
 //     return { 'Authorization': Authorization, 'X-Date': GMTString /*,'Accept-Encoding': 'gzip'*/}; //如果要將js運行在伺服器，可額外加入 'Accept-Encoding': 'gzip'，要求壓縮以減少網路傳輸資料量
 // }
-
+//-------------------------------------------------------------------------------------------
 // let data;
 
 
@@ -80,20 +80,20 @@
 //   let data = res.data;
 //  console.log(data); document.querySelector('.container').innerHTML = `
 //  <h1>${data[0].ScenicSpotName}</h1>
-//  <img src="${data[0].Picture.PictureUrl1}">` 
+//  <img src="${data[0].Picture.PictureUrl1}">`
 // })
 // ------------------------------------------------------------------------------------------------------
 // (function() {
 //     const BASE_URL = "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON/";
   
 //     axios.get(BASE_URL).then(response => {
-//     const data = response.data[0]; 
+//     const data = response.data[0];
 //     renderNavbarAvatar(data)
 //     console.log('data', data)
 //     })
-  
+
 //   })();
-  
+
 //   function renderNavbarAvatar(data) {
 //     console.log('data', data)
 //     const avatar = document.querySelector('[data-target="avatar-thumbnail"]')
@@ -103,12 +103,61 @@
 //   // <img src="${data.picture.thumbnail}" alt="avatar">
 //   //         <span>${data.name.first}</span>
 //   //         `
-    
+
 //     avatar.innerHTML = `
 //     <img src="${data.Picture.PictureUrl1}" alt="avatar">
 //     <span>${data.ScenicSpotName}</span>
 //   `
 //   }
 
-  
-  
+//https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taichung?%24filter=contains(ScenicSpotName%2C'%E5%B1%B1')&%24top=6&%24format=JSON
+//https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?%24filter=contains(ScenicSpotName%2C'%E5%B1%B1')&%24top=5&%24format=JSON
+//https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24filter=contains(ScenicSpotName%2C'%E5%B1%B1')&%24top=3&%24format=JSON
+const city = document.querySelector('.city');
+const keyword = document.querySelector('.keyword');
+const limit = document.querySelector('.limit');
+const send = document.querySelector('.send');
+const list = document.querySelector('.list');
+send.addEventListener('click',function(e){
+  const cityarea = city.value;
+  const keywordTxt = keyword.value; 
+  const limitNum = limit.value;
+
+  axios.get(
+   `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/${cityarea}?%24select=ScenicSpotName&%24filter=contains(ScenicSpotName%2C'${keywordTxt}')&%24top=${limitNum}&%24format=JSON
+`,
+   {
+      headers: getAuthorizationHeader()
+   }
+  )
+  .then(function (response) {
+    const thisData = response.data;
+    let str="";
+    thisData.forEach(item=>{
+      console.log(item)
+      str+=`<li>${item.ScenicSpotName}</li>`
+    })
+    list.innerHTML = str;
+  })
+  .catch(function (error) {
+    console.log(error);
+  }); 
+})
+
+
+
+
+function getAuthorizationHeader() {
+//   ID、KEY 開始
+    let AppID = 'd595bbed3b664388a6813b013e4a6d3a';
+    let AppKey = 'vB1_5kfk4LixTqh_YHdhx9XAAmY';
+//   ID、KEY 結束
+    let GMTString = new Date().toGMTString();
+    let ShaObj = new jsSHA('SHA-1', 'TEXT');
+    ShaObj.setHMACKey(AppKey, 'TEXT');
+    ShaObj.update('x-date: ' + GMTString);
+    let HMAC = ShaObj.getHMAC('B64');
+    let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+    return { 'Authorization': Authorization, 'X-Date': GMTString }; 
+}
+
